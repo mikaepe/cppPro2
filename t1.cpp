5bcfcc2fd9478068de35291789a1b9f516d4e820
Mikael Persson <mikaepe@kth.se>
2016-10-16 14:31:11 +0200

    myexp almost working!

--- t1.cpp
+++ t1.cpp
@@ -1,9 +1,14 @@
+/* SF2565 Project 2, Hanna Hultin & Mikael Persson.
+ * Task 1: Compute exp(x) for real numbers
+ */
 
 
 // Libraries	::	::	::	::	::	::
+
 #include<iostream>
 #include<cmath>
 #include<iomanip>
+
 
 // Function Declarations 	::	::	::	::
 
@@ -23,9 +28,12 @@ int main(int argc, char *argv[])
   double x;
   cout << "x = "; cin >> x;
   cout << fixed << setprecision(12);
+  
   double expx = exp(x);
+  cout << "cmath: exp(x) = " << expx << endl;
 
-  cout << "cmath:  exp(x) = " << expx << endl;
+  double myexpx = myexp(x);
+  cout << "myexp: exp(x) = " << myexpx << endl;
 
 
   return 0;
@@ -33,7 +41,7 @@ int main(int argc, char *argv[])
 
 /* Function myexp is an implementation of the exponential
  * functions series representation i.e. the McLaurin
- * series.
+ * series. Horners algorithm used as in proj. 1.
  */
 double myexp(double x, double tol)
 {
@@ -41,7 +49,7 @@ double myexp(double x, double tol)
   res = 1;
   for (int i = 10; i > 0; i--)
   {
-    res = 1+x*res/i;
+    res = 1+x*res/i;			// TODO : Använd tol på lämpligt vis
   }
   return res;
 }
