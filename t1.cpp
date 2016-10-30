8785c3b4de92964d58cc57c250cefb2d070bbe71
Mikael Persson <mikaepe@kth.se>
2016-10-30 11:14:24 +0100

    fixed myexp in t1 to handle negative x

--- t1.cpp
+++ t1.cpp
@@ -12,84 +12,64 @@
 
 // Function Declarations 	::	::	::	::
 
-double myexp(double x, double tol=1e-14);
-double myexpWoH(double x, double tol=1e-14);
-
+double myexp(double x, double tol=1e-10);	
+double myexpHorner(double x);
 
 // Function Definitions		::	::	::	::
 
 using namespace std;
 
 /* main function, calls myexp and compares with cmath exp
- * blah
- * blah
  */
 int main(int argc, char *argv[])
 {
   double x;
   cout << "x = "; cin >> x;
-  cout << fixed << setprecision(15);
+  cout << fixed << setprecision(12);
   
 
   double expx = exp(x);
+  double myexpH = myexpHorner(x);
   double myexpx = myexp(x);
-  double myexpxwoH = myexpWoH(x);
 
-  cout << "cmath:    exp(x) = " << expx << endl;
-  cout << "myexp:    exp(x) = " << myexpx << endl;
-  cout << "myexpwoH: exp(x) = " << myexpxwoH << endl;
-
-
+  cout << "cmath:  exp(x) = " << expx << endl;
+  cout << "myexp:  exp(x) = " << myexpx << endl;
+  cout << "myexpH: exp(x) = " << myexpH << endl;
+ 
   return 0;
 }
 
 
-/* NEW IMPLENTATION OF HORNERS WITH TOL
- */
-double myexp(double x, double tol)
-{
-  int req_iter = 2;			// requested iterations
-  double res1 = 1, res2 = 0;		// initialize values to start looping
-  int maxiter = 100;			// max iterations to be performed
-
-  // stop when the last term is less than tolerance i.e. abs(res1-res2)
-  while (req_iter < maxiter && abs(res1-res2) > tol)
-  {
-    req_iter ++;
-    res2 = res1;
-    res1 = 1;
-    for (int i = req_iter; i>0; i--)
-    {					// Horners algorithm (to avoid
-      res1 = 1+x*res1/i;		// adding small doubles to large)
-    }
-  }
-
-  if (req_iter == maxiter)
-  {
-    cout << "max iterations performed, tolerance not attained" << endl;
-  }
-  cout << "iterations performed (Horners): " << req_iter << endl;
-
-  return res1;
-}
-
-
-/* Function myexpWoH is an straight forward implementation of the exponential
+/* Function myexp is an straight forward implementation of the exponential
  * functions series representation i.e. the McLaurin
  * series without Horners algorithm
  */
-double myexpWoH(double x, double tol)
+double myexp(double x, double tol)
 {
     double res = 1;
     double term = 1;
     for (int i = 1; i<1000; i++)
     {
         term *= (x/ (double) i);
-        if (term < tol){
+        if (abs(term) < tol){
 	  cout << "iterations performed     (WoH): " << i << endl;
 	  break;
         }
         res += term;
     }
     return res;
+}
+
+/* Implementation of Horners algorithm to compare with
+ */
+double myexpHorner(double x)
+{
+  double res = 1;
+  for (int i = 30; i>0; i--)
+  {					// Horners algorithm (to avoid
+    res = 1+x*res/i;		// adding small doubles to large)
+  }
+  
+
+  return res;
 }
